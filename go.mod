d87159380c3595d8f1b1fb79ccdcad54e618df97
Angelo Lloti <angelolloti@Angelos-MacBook-Air.local>
2023-11-09 09:37:17 -0500

    add firebase auth, add db logging, change uuid from string to UUID type in pg

--- go.mod
+++ go.mod
@@ -3,6 +3,16 @@ module bigbox
 go 1.20
 
 require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute v1.18.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/firestore v1.9.0 // indirect
+	cloud.google.com/go/iam v0.13.0 // indirect
+	cloud.google.com/go/longrunning v0.4.1 // indirect
+	cloud.google.com/go/storage v1.30.1 // indirect
+	firebase.google.com/go v3.13.0+incompatible // indirect
+	firebase.google.com/go/v4 v4.12.1 // indirect
+	github.com/MicahParks/keyfunc v1.9.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -16,7 +26,13 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/google/uuid v1.4.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
@@ -32,11 +48,21 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.4 // indirect
 	github.com/vmihailenco/tagparser v0.1.2 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.6.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/api v0.114.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/appengine/v2 v2.0.2 // indirect
+	google.golang.org/genproto v0.0.0-20230320184635-7606e756e683 // indirect
+	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	mellium.im/sasl v0.3.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net/http"
@@ -8,40 +9,69 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
+	firebase "firebase.google.com/go/v4"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/go-pg/pg/v10"
 	"github.com/go-pg/pg/v10/orm"
 	"github.com/google/uuid"
+	"google.golang.org/api/option"
 )
 
 const FILES = "./files"
 
 type File struct {
-	ID          string `json:"id"`
-	Size        int64  `json:"size"`
-	Filename    string `json:"name"`
-	ContentType string `json:"type"`
+	ID          uuid.UUID `json:"id" pg:",type:uuid"`
+	UserID      string    `json:"user_id" pg:",notnull"`
+	CreatedAt   time.Time `json:"created_at" pg:"default:now()"`
+	Size        int64     `json:"size"`
+	Filename    string    `json:"name"`
+	ContentType string    `json:"type"`
 }
 
 var db *pg.DB
 var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
 
+type dbLogger struct{}
+
+func (d dbLogger) BeforeQuery(c context.Context, q *pg.QueryEvent) (context.Context, error) {
+	return c, nil
+}
+
+func (d dbLogger) AfterQuery(c context.Context, q *pg.QueryEvent) error {
+	l, _ := q.FormattedQuery()
+	fmt.Println(string(l))
+	return nil
+}
+
 func main() {
+	app, err := firebase.NewApp(context.Background(), nil, option.WithCredentialsFile("private_keys.json"))
+	if err != nil {
+		panic(err)
+	}
+
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
 	r := gin.Default()
 	db = pg.Connect(&pg.Options{
 		User:     "postgres",
 		Password: "epic",
 	})
 	defer db.Close()
+	db.AddQueryHook(dbLogger{})
 
 	models := []interface{}{
 		(*File)(nil),
 	}
 
 	for _, model := range models {
+		db.Model(model).DropTable(&orm.DropTableOptions{IfExists: true})
 		err := db.Model(model).CreateTable(&orm.CreateTableOptions{
 			IfNotExists: true,
 		})
@@ -50,23 +80,38 @@ func main() {
 		}
 	}
 
+	if _, err := db.Exec(`CREATE INDEX IF NOT EXISTS idx_user_id ON files (user_id)`); err != nil {
+		panic(err)
+	}
+
 	r.Use(static.Serve("/", static.LocalFile("static", false)))
 
 	r.GET("/files/:id", func(c *gin.Context) {
-		file := File{ID: c.Param("id")}
-
-		err := db.Model(&file).Select()
+		id, err := uuid.Parse(c.Param("id"))
 		if err != nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		file := File{ID: id}
+
+		if err := db.Model(&file).WherePK().Select(); err != nil {
 			c.AbortWithStatus(http.StatusNotFound)
 			return
 		}
 
 		c.Header("Content-Type", file.ContentType)
 		c.Header("Content-Disposition", "filename=\""+quoteEscaper.Replace(file.Filename)+"\"")
-		c.File(filepath.Join(FILES, file.ID))
+		c.File(filepath.Join(FILES, file.ID.String()))
 	})
 
 	r.POST("/files", func(c *gin.Context) {
+		authToken, err := client.VerifyIDToken(context.Background(), c.Request.FormValue("token"))
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
 		header, err := c.FormFile("file")
 		if err != nil {
 			c.AbortWithError(http.StatusBadRequest, err)
@@ -83,8 +128,8 @@ func main() {
 
 		fmt.Println("copying " + header.Filename)
 
-		id := uuid.NewString()
-		localFile, err := os.Create(filepath.Join(FILES, id))
+		id := uuid.New()
+		localFile, err := os.Create(filepath.Join(FILES, id.String()))
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
@@ -102,14 +147,15 @@ func main() {
 			return
 		}
 
-		f := &File{
+		f := File{
 			ID:          id,
+			UserID:      authToken.UID,
 			Size:        header.Size,
 			Filename:    s,
 			ContentType: header.Header.Get("Content-Type"),
 		}
 
-		_, err = db.Model(f).Insert()
+		_, err = db.Model(&f).Insert()
 		if err != nil {
 			c.AbortWithError(http.StatusInternalServerError, err)
 			return
